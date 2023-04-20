fbe604049a478ea4d4dff4126e6f0b4d50c4f54f
Varensev Vitaliy <vitalik.unit@gmail.com>
2023-04-20 15:36:36 +0500

    fix import img

--- src/components/icon-home.tsx
+++ src/components/icon-home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function IconHome() {
     return (
         <div className="w-[64px] h-[64px] rounded-full bg-[#F4F4F4] flex items-center justify-center ">
-            <img className="w-[35px] h-[35px]" src="public/img/house.png" alt=""/>
+            <img className="w-[35px] h-[35px]" src="/img/house.png" alt=""/>
         </div>
     );
 }
--- src/components/scrollbar-image.tsx
+++ src/components/scrollbar-image.tsx
@@ -4,16 +4,16 @@ function ScrollbarImage() {
     return (
         <div className="flex gap-[12px] w-full overflow-x-scroll mb-[32px]">
 
-                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="../../public/img/Rectangle 23.jpg" alt=""/>
+                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="/img/Rectangle 23.jpg" alt=""/>
 
 
-                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="../../public/img/Rectangle 24.jpg" alt=""/>
+                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="/img/Rectangle 24.jpg" alt=""/>
 
 
-                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="../../public/img/Rectangle 28.jpg" alt=""/>
+                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="/img/Rectangle 28.jpg" alt=""/>
 
 
-                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="../../public/img/Rectangle 23.jpg" alt=""/>
+                <img className="w-[164px] h-[149px] rounded-xl drop-shadow-lg bg-[#FFFFFF]" src="/img/Rectangle 23.jpg" alt=""/>
 
         </div>
     );
