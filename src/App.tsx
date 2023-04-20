a809dc98bc64a131be6fcd9979e614c05fe43cbc
Varensev Vitaliy <vitalik.unit@gmail.com>
2023-04-20 14:30:39 +0500

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
             <Router>
                 <Routes>
-                    <Route exact path="/" element={<HomePage />} />
+                    <Route  path="/" element={<HomePage />} />
                     <Route path="/login" element={<Login />} />
                 </Routes>
             </Router>
